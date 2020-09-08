56f051d56c583daa1b31b39c40b192d7400e58fb
abhishekpro-tech <abhisheksharma.sharma039@gmail.com>
2020-09-08 20:24:06 +0530

    My first landing page

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<meta name = "robats"content="index,nofollow" />
+<title>Landing Page</title>
+</head>
+
+<!--top header start-->
+<div class="row">
+	<div class="col-sm-12">
+    <div class="top_header"></div>
+    </div>
+</div>
+
+<body>
+
+
+</body>
+</html>
